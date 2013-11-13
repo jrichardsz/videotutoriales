83a70aec9001fce157e9877fb0bdbeba98ebe542
Richard Leon Ingaruca <import.linet@gmail.com>
2013-11-13 02:12:47 -0500

    simple hola mundo android!!

---
+++ 000018/resources/hola-mundo-android/src/org/example/MainActivity.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello wordl Android by JRichardsz");
+        setContentView(tv);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
